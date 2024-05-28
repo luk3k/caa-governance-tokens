25973f6f2f0efee7bf426bff56f95ac871f651f2
Manuel Kuß <e11722667@student.tuwien.ac.at>
2024-05-28 11:40:40 +0200

    1st: added threaded version get_past_logs_threaded; 2nd: implemented perform_get_logs_with_too_many_requests_protection ; 2nd: changed get_past_logs to use step-size from beginning and don't wait for ValueError

--- src/read_data.py
+++ src/read_data.py
@@ -1,3 +1,6 @@
+import csv
+import os
+import threading
 from argparse import ArgumentParser
 import json
 import time
@@ -7,13 +10,21 @@ import yaml
 import pandas as pd
 from web3 import Web3
 from web3._utils.events import get_event_data
+from requests.exceptions import HTTPError
 
 with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 analysis_end_block = 19955500
+analysis_start_block = 10861674 # uniswap first block with logs
+
+analysis_end_block_test_multi_threading = 19955500
+analysis_start_block_test_multi_threading = 19900000 # end - start = 55500 blocks
+analysis_block_range_test_multi_threading = (analysis_end_block_test_multi_threading - analysis_start_block_test_multi_threading)
 block_run_limit = 50_000
 
+nThreads = 3
+step_size = 200
 
 # connection to the ethereum node
 def conETH(infura_api):
@@ -26,23 +37,163 @@ def conETH(infura_api):
     except:
         return None
 
+# work is distributed to threads
+def get_past_logs_threaded(w3, address, topic, file_path, num_threads):
+    tmp_files = [file_path.split(".")[0] + "_" + str(i) + ".csv" for i in range(num_threads)]
+    threads = []
+
+    # --- thread block range allocation example ---
+    # start block: 19900000
+    # end block: 19955500
+    # diff: 55500
+    #
+    # thread 1 part: 19900000 - 19927749
+    # thread 2 part: 19927750 - 19955500
+    
+    thread_block_rate = analysis_block_range_test_multi_threading/num_threads
+
+    for i in range(num_threads):
+        start_block = int(analysis_start_block_test_multi_threading + i * thread_block_rate)
+        end_block = int(analysis_start_block_test_multi_threading + (i+1) * thread_block_rate - 1)
+
+        if (i == num_threads - 1):
+            end_block = end_block + 1   # add one more for the last thread range:
+
+        print(f"thread {i}: start block: {start_block}, end block: {end_block})")
+        threads.append(threading.Thread(target=get_past_logs_thread, args=(w3, address, topic, tmp_files[i], start_block, end_block, i)))
+
+    # start threads
+    for i in range(num_threads):
+        threads[i].start()
+
+    # wait for threads to finish
+    for i in range(num_threads):
+        threads[i].join()
+
+    # merge tmp csv files
+    merge_csv_files(tmp_files, file_path)
+
+def merge_csv_files(files, final_file):
+
+    # somehow replacing the file does not work with open mode "w" ??
+    # therefore i remove it, when it exists
+    if os.path.exists(final_file):
+        os.remove(final_file)
+
+    with open(final_file, mode='w') as f1:
+        writer = csv.writer(f1)
+        f1.write("tx_hash,block_number,from,to,amount\n")
+
+        # write tmp files to final_file
+        for file in files:
+            with open(file, mode='r') as f2:
+                reader = csv.reader(f2)
+                for row in reader:
+                    writer.writerow(row)
+
+        # remove tmp files
+        for file in files:
+            if os.path.exists(file):
+                os.remove(file)
+
+def perform_get_logs_with_too_many_requests_protection(w3, filter, thread_num):
+    sleeping_time = 1
+    done = False
+    while not done:
+        try:
+            result = w3.eth.get_logs(filter)
+        except HTTPError as e:
+            if e.response.status_code == 429:
+                print(f"[thread {thread_num}] Too many request error. Retrying in {sleeping_time} seconds...")
+                time.sleep(sleeping_time)
+                sleeping_time *= 2
+
+            time.sleep(sleeping_time)
+            sleeping_time *= 2
+            continue
+        done = True
+    return result
+
+# each thread calls this function
+def get_past_logs_thread(w3, address, topic, file_path, start_block, end_block, thread_num):
+    num_of_blocks = 0
+
+    from_block = start_block
+    to_block = from_block + step_size
+
+    while True:
+        try:
+            start = time.time_ns()
+            if from_block >= end_block:
+                break
+            if to_block > end_block:
+                to_block = end_block
+
+            filter = {
+                'fromBlock': from_block,
+                'toBlock': to_block,
+                'address': address,
+                'topics': [topic]
+            }
+
+            print(f"[thread {thread_num}] from block: {from_block}; to block: {to_block};")
+            print(f"[thread {thread_num}] number of blocks: {to_block - from_block}")
+
+            logs = perform_get_logs_with_too_many_requests_protection(w3, filter, thread_num)
+            end_request = time.time_ns()
+            num_of_blocks = num_of_blocks + (to_block - from_block)
+
+            df = pd.DataFrame()
+
+            print(f'[thread {thread_num}] len(logs): {len(logs)}')
+            for l in logs:
+                filtered_log = {
+                    'tx_hash': [l['transactionHash'].hex()],
+                    'block_number': [l['blockNumber']],
+                    'from': [l['topics'][1].hex()],
+                    'to': [l['topics'][2].hex()],
+                    'amount': [get_uint(l['data'].hex()[2:])]
+                }
+                new_df = pd.DataFrame.from_dict(filtered_log)
+                df = pd.concat([df, new_df])
+
+            df.to_csv(file_path, index=False, header=False, mode='a')
+            end_total = time.time_ns()
+
+            print(f'[thread {thread_num}] Time for request: {(end_request - start) / 1_000_000} ms')
+            print(f'[thread {thread_num}] Time total: {(end_total - start) / 1_000_000} ms')
+            print(f'[thread {thread_num}] Total number of bloks: {num_of_blocks}')
+
+            from_block = to_block + 1
+            to_block = from_block + step_size
+
+        # ValueError is used to get earliest from_block value
+        except ValueError as ve:
+            print('ValueError: ', ve)
+            break
+        except RuntimeError as e:
+            print('Error getting logs: ', e)
+            break
+
+    print(f"Last block: {from_block - 1}")
+
 
 # get transaction by hash
-def get_past_logs(w3, address, topic, start_block, file_path):
+def get_past_logs(w3, address, topic, start_block, end_block, file_path):
     done = False
     # result = pd.DataFrame()
-    step_size = 100
     num_of_blocks = 0
 
     from_block = "earliest" if start_block is None else int(start_block)
-    to_block = analysis_end_block
-    while num_of_blocks < block_run_limit:
+    # to_block = analysis_end_block
+    to_block = from_block + step_size
+    while True: #num_of_blocks < block_run_limit:
         try:
             start = time.time_ns()
-            if type(from_block) is not str and from_block >= analysis_end_block:
+            if type(from_block) is not str and from_block >= end_block:
                 break
-            if to_block > analysis_end_block:
-                to_block = analysis_end_block
+            if to_block > end_block:
+                to_block = end_block
 
             filter = {
                 'fromBlock': from_block,
@@ -86,6 +237,8 @@ def get_past_logs(w3, address, topic, start_block, file_path):
             print(f'Total number of bloks: {num_of_blocks}')
             print('\n')
 
+            if to_block == end_block:
+                break
             from_block = to_block + 1
             # to_block = analysis_end_block
             to_block = from_block + step_size
@@ -93,11 +246,13 @@ def get_past_logs(w3, address, topic, start_block, file_path):
         # ValueError is used to get earliest from_block value
         except ValueError as ve:
             print('ValueError: ', ve)
-            from_block = int(ve.args[0]["data"]["from"], 16)
+            # from_block = int(ve.args[0]["data"]["from"], 16)
             # to_block = int(ve.args[0]["data"]["to"], 16)
-            to_block = from_block + step_size
+            # to_block = from_block + step_size
+            break
         except RuntimeError as e:
             print('Error getting logs: ', e)
+            break
 
     print(f"Last block: {from_block - 1}")
     # return result
@@ -114,7 +269,7 @@ def get_int(data):
 def split_data(data):
     return [data[i:i + 64] for i in range(2, len(data), 64)]
 
-
+# run with: -a 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984 -t 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef -o data/output.csv
 def main(args):
     start_time = time.time()
     if args.topic is None:
@@ -122,17 +277,23 @@ def main(args):
     if args.address is None:
         print('Error: address not specified')
 
-    # init csv headers
-    if args.start_block is None:
-        with open(args.output, 'w') as f:
-            f.write("tx_hash,block_number,from,to,amount\n")
+    # --- old code for get_past_logs ---
+    # # init csv headers
+    # if args.start_block is None:
+    #     with open(args.output, 'w') as f:
+    #         f.write("tx_hash,block_number,from,to,amount\n")
+    # ---
 
     # connect to the Ethereum node
     if config["keys"]["infura_apikey"] is not None:
         eth_con = conETH(config["keys"]["infura_apikey"])
         # make the query
         if eth_con is not None:
-            get_past_logs(eth_con, args.address, args.topic, start_block=args.start_block, file_path=args.output)
+            # --- old code for get_past_logs ---
+            # get_past_logs(eth_con, args.address, args.topic, analysis_start_block_testing, analysis_end_block, file_path=args.output)
+            # ---
+
+            get_past_logs_threaded(eth_con, args.address, args.topic, args.output, nThreads)
         else:
             print("Error: connection to the ethereum node failed")
             return
