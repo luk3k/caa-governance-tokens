5f91db0660eb15b8c247a27b9749365d1dd1b605
luk3k <luke.kuess@gmail.com>
2024-06-03 12:56:48 +0200

    added gini coefficient, bar plot and pie chart

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,53 +1,83 @@
-#!/usr/bin/python
 from argparse import ArgumentParser
 from decimal import Decimal
 
+from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 
-def analyze_data(file, out):
-    pd.options.display.max_colwidth = 68
-    pd.set_option('float_format', '{:f}'.format)
 
-    df = pd.read_csv(file)
-    df["amount"] = df["amount"].apply(Decimal)
+def create_pie_chart(df):
+    plt.figure()
+    data = df["balance"].to_numpy()
+    addresses = df["address"]
+    plt.pie(data, labels=addresses)
+    plt.savefig("data/uniswap_pie_chart.png")
 
-    print("unique columns: ", df.nunique())
 
-    to_group = df.groupby(['to']).agg({'amount': 'sum'})
-    to_group.index.names = ['address']
-    # print("to unique: ", to_group.nunique())
-    from_group = df.groupby(['from']).agg({'amount': 'sum'})
-    from_group.index.names = ['address']
-    # print("from unique: ", from_group.nunique())
+def create_bar_chart(df):
+    plt.figure()
+    addresses = df["address"]
+    data = df["balance"].to_numpy()
+    plt.barh(addresses, data)
+    plt.savefig("data/uniswap_bar_chart.png")
 
-    df_total = pd.merge(to_group, from_group, left_index=True, right_index=True, suffixes=("_in", "_out"), how="outer").fillna(0)
-    df_total["balance"] = df_total["amount_in"] - df_total["amount_out"]
-    # df_total
+# https://en.wikipedia.org/wiki/Herfindahl%E2%80%93Hirschman_index
+def compute_herfindahl_hirschman_index(df):
+    # TODO
+    pass
 
-    df_total_negativeBalance = df_total.loc[df_total["balance"] < 0]
+def compute_gini_index(df):
+    balances = df["balance"].to_numpy()
+    print("gini index: ", gini(balances))
 
-    # df['total_tokens'] =
-    # print(to_group)
-    # print(from_group)
-    print(df_total)
-    df_total.to_csv(out)
-    print(df_total_negativeBalance)
+
+# gini index info: lies between 0 (max equality) and 1 (max inequality)
+# gini reference values: https://en.wikipedia.org/wiki/Gini_coefficient
+# source: https://www.statology.org/gini-coefficient-python/
+def gini(x):
+    total = 0
+    for i, xi in enumerate(x[:-1], 1):
+        total += np.sum(np.abs(xi - x[i:]))
+    return total / (len(x) ** 2 * np.mean(x))
+
+
+# remove negative values
+def clean_data(df):
+    non_negative_balances = df.loc[df["balance"] >= 0]
+    return non_negative_balances.sort_values(by=["balance"], ascending=False)
+
+
+def merge_others_at_cut_off_value(df, cut_point):
+    df_prepared = df.head(cut_point)
+    df_rest = df.iloc[cut_point:]
+    sum_others = df_rest["balance"].sum()
+    # print("sum: ", sum_others)
+    row = pd.DataFrame({"address": "others", "balance": sum_others}, index=[0])
+    return pd.concat([df_prepared, row], ignore_index=True)
 
 
 def main(args):
     if args.file is None:
         print('Error: Please specify the file to analyze')
 
-    analyze_data(args.file, args.out)
+    df = pd.read_csv(args.file)
+    df["balance"] = df["balance"].apply(Decimal)
+    df["amount_in"] = df["amount_in"].apply(Decimal)
+    df["amount_out"] = df["amount_out"].apply(Decimal)
+
+    cleaned_df = clean_data(df)
+    processed_df = merge_others_at_cut_off_value(cleaned_df, 20)
+    create_pie_chart(processed_df)
+    create_bar_chart(processed_df)
+    compute_gini_index(cleaned_df)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--file',
                         help='Path for file', type=str, required=True)
-    parser.add_argument('--out', '-o',
-                        help='Path for output file', type=str, required=True)
+    # parser.add_argument('--out', '-o',
+    #                     help='Path for output file', type=str, required=True)
 
     args = parser.parse_args()
-
     main(args)
